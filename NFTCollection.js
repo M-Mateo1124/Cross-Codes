6e79536b79a8787345819382a40cd7cf5d925fe1
M-Mateo1124 <141466241+M-Mateo1124@users.noreply.github.com>
2023-08-29 08:30:52 +0800

    Update NFTCollection.js
    A code that's mainly Javascript, it focuses on being able to input and mint NFTs

--- NFTCollection.js
+++ NFTCollection.js
@@ -1,32 +1,53 @@
+// variables that allows the user to input their names, eye color, shirt type, bling 
+
+var userName = prompt("Enter your Name: "); 
+
+var userEyeColor = prompt("Enter your Eye Color: "); 
+
+var userShirtType = prompt("Pick a Shirt Type (T-Shirt, Long Sleeve, Hoodie): "); 
+
+var userAccessory = prompt("Pick an Accessory(Gold Chains, Diamond Ear-Piercings, Silver Rings): "); 
+
+// Divider between the input and output
+console.log("--------------------------------") 
+
+// create a variable to hold your NFT's
 const NFTs = []
-
-function mintNFT (name, eyecolor, shirttype, bling) {
-   const NFT = {
-      "Name": name,
-      "EyeColor": eyecolor,
-      "ShirtType": shirttype,
-      "Bling": bling
-   }
-   NFTs.push(NFT);
-   console.log("Minted: "+ name);
+// this function will take in some values as parameters, create an
+// NFT object using the parameters passed to it for its metadata, 
+// and store it in the variable above.
+function mintNFT (name, eyecolor, shirttype, accessory) {
+   const NFT = {
+      "Name": name,
+      "EyeColor": eyecolor,
+      "ShirtType": shirttype,
+      "Accessory": accessory
+   }
+   NFTs.push(NFT);
+   console.log("Minted: "+ name); 
 
 }
-
+// create a "loop" that will go through an "array" of NFT's
+// and print their metadata with console.log()
 function listNFTs () {
-   for(let i = 0; i < NFTs.length; i++) {
-      console.log("ID: " + (i + i));
-      console.log("Name: " + NFTs[i].Name);
-      console.log("Eye Color: " + NFTs[i].EyeColor);
-      console.log("Shirt Type: " + NFTs[i].ShirtType);
-      console.log("Bling: " + NFTs[i].Bling);
-   }
+   for(let i = 0; i < NFTs.length; i++) {
+      console.log("ID: " + (i + i));
+      console.log("Name: " + NFTs[i].Name);
+      console.log("Eye Color: " + NFTs[i].EyeColor);
+      console.log("Shirt Type: " + NFTs[i].ShirtType);
+      console.log("Accessory: " + NFTs[i].Accessory);
+   } 
 
-}
+} 
 
+// print the total number of NFTs we have minted to the console
 function getTotalSupply() {
-   console.log(NFTs.length);
+   console.log(NFTs.length);
 }
 
-mintNFT ("Cross", "Red", "LongSleeve", "Rings");
+
+// call your functions below this line 
+
+mintNFT (userName, userEyeColor, userShirtType, userAccessory);
 listNFTs();
 getTotalSupply();
